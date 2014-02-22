663b10b3023a1236f7c1da5d279d3e0c53720bba
Torben Rasmussen <rtorben@gmail.com>
2014-02-22 14:40:35 -0800

    remove foo

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,10 +1,5 @@
 (ns euler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 ;; 1
 
 ; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
